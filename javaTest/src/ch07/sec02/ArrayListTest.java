c5b84abf3184baf4a87766316c873bc084ed0f77
Woo Jin Han <wjjh23@gmail.com>
2023-09-26 11:54:57 +0900

    [Updated ArrayListTest] Added subtitles and added test to change ArrayList to Iterator

--- javaTest/src/ch07/sec02/ArrayListTest.java
+++ javaTest/src/ch07/sec02/ArrayListTest.java
@@ -2,6 +2,7 @@ package ch07.sec02;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class ArrayListTest {
@@ -11,6 +12,7 @@ public class ArrayListTest {
 		String[] strArray = new String[10];
 		ArrayList<String> strList = new ArrayList<String>(); // automatically creates ten elements
 		
+		System.out.println("*PRINT ARRAY / ARRAYLIST*");
 		// assign random numbers to indices
 		for(int i = 0; i < strArray.length; i++) {
 			int no = (int)(Math.random()*(100)+(1));
@@ -18,13 +20,24 @@ public class ArrayListTest {
 			strList.add(new String("Num " + no));
 		}
 		printArrayAndList(strArray, strList);
-		System.out.println("*******************************************");
+		System.out.println();
+		System.out.println("*ITERATOR*");
+		
+		// change ArrayList to Iterator
+		Iterator<String> iterator = strList.iterator();
+		while(iterator.hasNext()) {
+			String data = iterator.next();
+			System.out.printf("<%s> ", data);
+		}
+		System.out.println();
+		System.out.printf("%n*INSERT ELEMENT*%n");
 		
 		// insert "element" into first index of array and ArrayList
 		strArray[1] = new String("element"); // this will replace index [1] and leave the others untouched
 		strList.add(1, "element"); // this will push back the indices after [1] by one element
 		printArrayAndList(strArray, strList);
-		System.out.println("*******************************************");
+		System.out.println();
+		System.out.println("*SEARCH INDICES*");
 		
 		// search indices for "element" in array and return index
 		boolean isFound = false;
@@ -41,28 +54,28 @@ public class ArrayListTest {
 		System.out.printf("\"element\" is %sin the ArrayList", 
 				strList.contains("element") ? "" : "not ");
 		if(strList.contains("element")) System.out.printf(" at index %d%n", strList.indexOf("element"));
-		System.out.println("*******************************************");
+		System.out.println();
+		System.out.println("*CHANGE TO ARRAY / ARRAYLIST*");
 
 		// change ArrayList to an array and use enhanced for loop to print
 		Object[] obj = strList.toArray();
 		for(Object data : obj) {
 			System.out.printf("[%s] ", data.toString());
 		}
-		System.out.println("\n*******************************************");
+		System.out.println();
 		// change array to ArrayList
 		List<String> list = Arrays.asList(strArray);
 		for(String data : list) {
 			System.out.printf("<%s> ", (data != null) ? data.toString() : null);
 		}
-		System.out.println();
-		System.out.println("*******************************************");
-		
-		
+		System.out.printf("%n%n*REMOVE ELEMENT*%n");
+				
 		// remove index: array can only be changed to null; ArrayList element can be removed with remove()
 		strArray[1] = null;
 		strList.remove(1);
 		printArrayAndList(strArray, strList);
-		System.out.println("*******************************************");
+		System.out.println();
+		System.out.println("*CLEAR ARRAY / ARRAYLIST*");
 		
 		// clear indices: array can only be changed to null; ArrayList elements can be removed with clear()
 		for(int i = 0; i < strArray.length; i++) {
@@ -73,7 +86,8 @@ public class ArrayListTest {
 		System.out.printf("strArray.length = %d%n", strArray.length); // size of array not changed
 		System.out.printf("strList.size() = %d%n", strList.size()); // size of ArrayList is 0
 		
-		System.out.println("*******************************************");
+		System.out.println();
+		System.out.println("*CHECK FOR EMPTY ARRAY / ARRAYLIST*");
 		
 		// test if array is null
 		boolean isEmpty = true;
@@ -85,6 +99,7 @@ public class ArrayListTest {
 		System.out.printf("strList: %s%n", (strList.isEmpty() == true) ? 
 				"The list is empty" : "Ths list is not empty");
 		
+		System.out.println();
 		System.out.println("Close");
 	}
 	
