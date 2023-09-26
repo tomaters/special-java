fd34d94652e7135b02bc9af15fbf7206af1fba8f
Woo Jin Han <wjjh23@gmail.com>
2023-09-26 16:47:42 +0900

    [Modified ArrayListTest] Tested sort and reverse-sort functions; further formatting

--- javaTest/src/ch07/sec02/ArrayListTest.java
+++ javaTest/src/ch07/sec02/ArrayListTest.java
@@ -2,6 +2,7 @@ package ch07.sec02;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -12,8 +13,8 @@ public class ArrayListTest {
 		String[] strArray = new String[10];
 		ArrayList<String> strList = new ArrayList<String>(); // automatically creates ten elements
 		
-		System.out.println("*PRINT ARRAY / ARRAYLIST*");
 		// assign random numbers to indices
+		System.out.println("*PRINT ARRAY / ARRAYLIST*");
 		for(int i = 0; i < strArray.length; i++) {
 			int no = (int)(Math.random()*(100)+(1));
 			strArray[i] = new String("Num " + no);
@@ -21,25 +22,35 @@ public class ArrayListTest {
 		}
 		printArrayAndList(strArray, strList);
 		System.out.println();
-		System.out.println("*ITERATOR*");
 		
 		// change ArrayList to Iterator
+		System.out.println("*ITERATOR*");
 		Iterator<String> iterator = strList.iterator();
 		while(iterator.hasNext()) {
 			String data = iterator.next();
 			System.out.printf("<%s> ", data);
 		}
 		System.out.println();
-		System.out.printf("%n*INSERT ELEMENT*%n");
+
+		// arrange ArrayList into order according to ASCII code
+		System.out.printf("%n*SORT*%n");
+		Collections.sort(strList);
+		printArrayAndList(strArray, strList);
+		
+		// arrange ArrayList in reverse order
+		System.out.printf("%n*REVERSE-SORT*%n");
+		Collections.reverse(strList);
+		printArrayAndList(strArray, strList);
 		
 		// insert "element" into first index of array and ArrayList
+		System.out.printf("%n*INSERT ELEMENT*%n");
 		strArray[1] = new String("element"); // this will replace index [1] and leave the others untouched
 		strList.add(1, "element"); // this will push back the indices after [1] by one element
 		printArrayAndList(strArray, strList);
 		System.out.println();
-		System.out.println("*SEARCH INDICES*");
 		
 		// search indices for "element" in array and return index
+		System.out.println("*SEARCH INDICES*");
 		boolean isFound = false;
 		int index = -1;
 		for(int i = 0; i < strArray.length; i++) {
@@ -55,9 +66,9 @@ public class ArrayListTest {
 				strList.contains("element") ? "" : "not ");
 		if(strList.contains("element")) System.out.printf(" at index %d%n", strList.indexOf("element"));
 		System.out.println();
-		System.out.println("*CHANGE TO ARRAY / ARRAYLIST*");
 
 		// change ArrayList to an array and use enhanced for loop to print
+		System.out.println("*CHANGE TO ARRAY / ARRAYLIST*");
 		Object[] obj = strList.toArray();
 		for(Object data : obj) {
 			System.out.printf("[%s] ", data.toString());
@@ -68,16 +79,16 @@ public class ArrayListTest {
 		for(String data : list) {
 			System.out.printf("<%s> ", (data != null) ? data.toString() : null);
 		}
-		System.out.printf("%n%n*REMOVE ELEMENT*%n");
 				
 		// remove index: array can only be changed to null; ArrayList element can be removed with remove()
+		System.out.printf("%n%n*REMOVE ELEMENT*%n");
 		strArray[1] = null;
 		strList.remove(1);
 		printArrayAndList(strArray, strList);
 		System.out.println();
-		System.out.println("*CLEAR ARRAY / ARRAYLIST*");
 		
 		// clear indices: array can only be changed to null; ArrayList elements can be removed with clear()
+		System.out.println("*CLEAR ARRAY / ARRAYLIST*");
 		for(int i = 0; i < strArray.length; i++) {
 			strArray[i] = null;
 		}
@@ -87,9 +98,9 @@ public class ArrayListTest {
 		System.out.printf("strList.size() = %d%n", strList.size()); // size of ArrayList is 0
 		
 		System.out.println();
-		System.out.println("*CHECK FOR EMPTY ARRAY / ARRAYLIST*");
 		
 		// test if array is null
+		System.out.println("*CHECK FOR EMPTY ARRAY / ARRAYLIST*");
 		boolean isEmpty = true;
 		for(int i = 0; i < strArray.length; i++)
 			if(strArray[i] != null) isEmpty = false;
