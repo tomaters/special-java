6af60f1001ce46b9fd367ec3b6d4b04968c58386
Woo Jin Han <wjjh23@gmail.com>
2023-09-27 10:26:20 +0900

    [Modified Student and StudentMain] Changed search function using ArrayList methods instead of a loop Overrode hashCode and modified overriding equals Added comments

--- javaTest/src/ch07/problems/Student.java
+++ javaTest/src/ch07/problems/Student.java
@@ -1,9 +1,20 @@
 package ch07.problems;
 
+import java.util.Objects;
+
 public class Student {
-	private String name, major;
+	private String name;
+	private String major;
 	private int id;
 	private double gpa;
+	
+	public Student() {
+		this(null, null, 0, 0.0);
+	}
+	
+	public Student(String name, int id) {
+		this(name, null, id, 0.0);
+	}
 	
 	public Student(String name, String major, int id, double gpa) {
 		super();
@@ -32,10 +43,21 @@ public class Student {
 				this.name, this.major, this.id, this.gpa);
 	}
 	
+	// need to compare objects (non-repeatable; [name & id] in this case) because hashSets have no repeating keys
+	// so override hashCode and equals
+	@Override 
+	public int hashCode() {
+		return Objects.hash(this.name, this.id);
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
-		Student student = (Student)obj;
-		if(this.name == student.name && this.name.equals(student.name)) return true;
+		Student stu = null;
+		if(!(obj instanceof Student)) {
+			return false;
+		}
+		stu = (Student)obj;
+		if(this.name.equals(stu.name) && this.id == stu.id) return true;
 		return false;
 	}
 }
--- javaTest/src/ch07/problems/StudentMain.java
+++ javaTest/src/ch07/problems/StudentMain.java
@@ -2,6 +2,7 @@ package ch07.problems;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 public class StudentMain {
@@ -10,27 +11,27 @@ public class StudentMain {
 	public static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) {
 		
-		ArrayList<Student> student = new ArrayList<Student>();
+		List<Student> studentList = new ArrayList<Student>(); // this works because List is a superclass of ArrayList
 		
+		System.out.printf("Enter student name, major, ID, and GPA for %d people:%n", NUM_STUDENTS);
 		for(int i = 0; i < NUM_STUDENTS; i++) {	
-			System.out.println("Enter student name, major, ID, and GPA:");
 			System.out.println(">>");
 			String name = scan.next();
 			String major = scan.next();
 			int id = scan.nextInt();
 			double gpa = scan.nextDouble();
-			student.add(new Student(name, major, id, gpa));
+			studentList.add(new Student(name, major, id, gpa));
 		}
 		for(int i = 0; i < NUM_STUDENTS; i++) {
 			System.out.println("----------------------------");
-			System.out.println(student.get(i));
+			System.out.println(studentList.get(i));
 		}
 		
-		for(;true;) {
+/*		for(;true;) {
 			boolean isFound = false;
-			Iterator<Student> iterator = student.iterator();
+			Iterator<Student> iterator = studentList.iterator();
 			System.out.println("----------------------------");
-			System.out.println("Enter student name ('x' to close): ");
+			System.out.println("Enter student name to search ('x' to close): ");
 			String name = scan.next();
 			if(name.toLowerCase().equals("x")) break;
 			while(iterator.hasNext()) {
@@ -43,7 +44,25 @@ public class StudentMain {
 			}
 			if(!isFound) System.out.println("That name is not in the system");
 		}
+*/		
+
+		// search using ArrayList methods instead of a loop
+		while(true) {
+			System.out.println("Enter student name to search ('x' to close):");
+			String name = scan.next().trim();
+			if(name.toLowerCase().equals("x")) break;
+			System.out.println("Enter student ID:");
+			int id = Integer.parseInt(scan.next().trim());
+			Student stu = new Student(name, id);
+			boolean isFound = studentList.contains(stu);
+			if(isFound) {
+				System.out.printf("%s%n", studentList.get(studentList.indexOf(stu)));
+				continue;
+			}
+			System.out.println("That student and ID is not in the system");
+		}
 		
+		scan.close();
 		System.out.println("Close");
 	}
 
